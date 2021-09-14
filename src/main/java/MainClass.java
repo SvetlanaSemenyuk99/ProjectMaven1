fd43d37c231740fa63818559ada429725c1b8aad
SvetlanaSemenyuk99 <SvetlanaSemenyuk99@users.noreply.github.com>
2021-09-14 22:06:05 +0300

    re

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -13,10 +13,10 @@ public class MainClass {
         driver.manage().window().maximize();
         driver.get("https://www.instagram.com/accounts/login/?source=auth_switcher");
 
-//        MainPage mainPage = new MainPage(driver);
-//        mainPage.register("ssemenuck", "12345679w", "Svetlana Semenyuk", "Semenyuk189@gmail.com");
+       // MainPage mainPage = new MainPage(driver);
+       //mainPage.register("ssemenuck", "12345679w", "Svetlana Semenyuk", "Semenyuk189@gmail.com");
         LogInPage logInPage = new LogInPage(driver);
-        logInPage.logInProfil ("svsemenuck", "123456789w");
+        logInPage.logInProfil ("svsemenuck", "*********");
 
 
     }
